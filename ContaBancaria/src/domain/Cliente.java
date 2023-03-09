8b02a25a612555100e6751e896dab3e523840a41
deborah oliveira <deborah oliveira@REN-TI-1.liner.local.com.br>
2023-03-09 15:35:04 -0300

    Conta

--- ContaBancaria/src/domain/Cliente.java
+++ ContaBancaria/src/domain/Cliente.java
@@ -1,5 +1,31 @@
 package domain;
 
-public class Cliente {
-
+public class Cliente extends Conta {
+	private int id;
+	private int cpf;
+	private int rg;
+	private String nome;
+	
+	
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public int getRg() {
+		return rg;
+	}
+	public void setRg(int rg) {
+		this.rg = rg;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
 }
--- ContaBancaria/src/domain/Conta.java
+++ ContaBancaria/src/domain/Conta.java
@@ -1,5 +1,51 @@
 package domain;
 
 public class Conta {
+	
+	private int numero;
+	private String titular;
+	private double saldo;
+	static int qtdcontas;
+	
+	public Conta(String pTitular) {
+		this.titular = pTitular;
+		qtdcontas++;
+		this.numero = qtdcontas;
+		this.saldo = 0;
+		
+	}
+	public void DadosBancarios() {
+		System.out.println("-- DADOS BANCARIO --");
+		System.out.println("NUMERO: "+this.numero);
+		System.out.println("TITULAR: "+this.titular);
+		System.out.println("SALDO: "+this.saldo);
+		System.out.println("--------------------");
+		System.out.println("");
+	}
+	
+	public void Depositar(double pValor) {
+		System.out.println("-- RELIZAR DEPOSITO --");
+		System.out.println("SALDO ATUAL: "+this.saldo);
+		
+		this.saldo += pValor;
+		
+		System.out.println("SALDO ATUALIZADO: "+this.saldo);
+		System.out.println("-- SALDO REALIZADO COM EXIDO --");
+		System.out.println("");
+	}
 
+	public void Sacar(double pValor) {
+		System.out.println("-- RELIZAR SAQUE --");
+		System.out.println("SALDO ATUAL: "+this.saldo);
+		if(pValor <= this.saldo) {
+			this.saldo -= pValor;
+			System.out.println("-- SALDO ATUALIZADO --");
+		}
+		else {
+			System.err.println("-- SALDO INSUFICIENTE --");
+		}
+		
+		System.out.println("-- SAQUE REALIZADO COM EXIDO --");
+		System.out.println("");
+	}
 }
