3ffc6cd04043f75b64e493eb5d2e1a42f74d09d0
deborah oliveira <deborah oliveira@REN-TI-1.liner.local.com.br>
2023-03-10 12:45:04 -0300

    TESTE

--- ContaBancaria/src/execute/MainExecute.java
+++ ContaBancaria/src/execute/MainExecute.java
@@ -2,19 +2,49 @@ package execute;
 
 import java.util.Scanner;
 
+import domain.Conta;
+
 public class MainExecute {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		String ttl;
 		int opcao;
+		double valor;
 		
 		System.out.println("DIGITE TITULAR DA CONTA:");
 		ttl = scn.nextLine();
 		
+		Conta c1 = new Conta(ttl);
+		
 		do {
+			System.out.println("------ MENU ------");
+			System.out.println("1) DADOS BANCARIO");
+			System.out.println("2) DEPOSITO");
+			System.out.println("3) SAQUE");
+			System.out.println("0) SAIR");
 			
-			System.out.println();
+			System.out.println("------------------");
+			System.out.println("OPCAO: ");
+			opcao = scn.nextInt();
+			
+			if(opcao == 1) {
+				c1.DadosBancarios();
+			}
+			
+			else if(opcao == 2) {
+				System.out.println("Digite valor Deposito: ");
+				valor = scn.nextDouble();
+				
+				c1.Depositar(valor);
+			}
+			else if(opcao == 3) {
+				System.out.println("Digite valor de Saque: ");
+				valor = scn.nextDouble();
+				
+				c1.Depositar(valor);
+			}
+
 		}while (opcao != 0 );
 
 	}
